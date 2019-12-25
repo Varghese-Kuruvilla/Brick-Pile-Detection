487a3a73c8553c0e136e8e91ace55fe1f0fc9cc8
Varghese P K <54114316+Varghese-Kuruvilla@users.noreply.github.com>
2019-12-25 14:32:57 +0530

    Servoing( added detection of UAV construction zone)

--- BrickZone_Util.py
+++ BrickZone_Util.py
@@ -9,6 +9,7 @@ import os
 import numpy as np
 import time
 import darknet
+import glob
 
 #ROS Dependencies
 import rospy
@@ -17,13 +18,17 @@ from cv_bridge import CvBridge,CvBridgeError
 from geometry_msgs.msg import PointStamped
 
 #For colour determination
-from color_label import colorlabel
+#from color_label import colorlabel
 
 #Global variables for debugging
 detected = 0
 not_detected = 0
 
 class BrickZone(object):
+    '''
+    Class for detecting UAV,UGV construction zone and brick pick up zones
+    '''
+
     def __init__(self):
         super(BrickZone, self).__init__()
 
@@ -43,6 +48,10 @@ class BrickZone(object):
         self.color_code = None 
         self.brick_pile_color = {1:"Red",2:"Blue",3:"Green",4:"Orange"}
 
+        
+        self.uav_constr_detections = [] #List containing detections for UAV construction zone
+        self.uav_constr_pos_ls = []
+        self.uav_constr_pos_conf = []
     def initialize_network(self, cfg_path, weights_path, meta_path):
 
         if not os.path.exists(cfg_path):
@@ -69,7 +78,7 @@ class BrickZone(object):
         self.net_width = darknet.network_width(self.netMain)
         self.net_height = darknet.network_height(self.netMain)
         #self.darknet_image = darknet.make_image(self.net_width, self.net_height, self.net_channels)
-        self.darknet_image = darknet.make_image(640,480,3) #Changed
+        self.darknet_image = darknet.make_image(1920,1080,3) #Changed
 
         if self.altNames is None:
             try:
@@ -93,7 +102,7 @@ class BrickZone(object):
                 pass
 
         #Initializing ros Node
-        rospy.init_node('listener', anonymous=True)
+        #rospy.init_node('listener', anonymous=True)
 
 
     def select_roi(self):                #Function to crop the image to BB dimensions
@@ -121,7 +130,27 @@ class BrickZone(object):
 
     #Both of these are helper functions used for drawing boxes on the objects
     def convertBack(self, x, y, w, h):
-        
+        '''
+        Converts yolo annotation format to pascal VOC format
+
+        Parameters
+
+        -------------
+
+        x,y,w,h:float
+                x,y: absolute x and y coordinates of the image centre
+                w,h: absolute width and height of the BB
+
+        -------------
+
+        Returns
+
+        -------------
+
+        xmin,ymin,xmax,ymax:int
+                            xmin,ymin: x and y coordinates of the top left corner of the BB
+                            xmax,ymax: x and y coordinates of the bottom right corner of the BB
+        '''
         xmin = int(round(x - (w / 2)))
         xmax = int(round(x + (w / 2)))
         ymin = int(round(y - (h / 2)))
@@ -129,7 +158,23 @@ class BrickZone(object):
         return xmin, ymin, xmax, ymax
 
     def cvDrawBoxes(self ,img):
-        
+        '''
+        Function to draw rectangles around the detected classes
+
+        Parameters
+
+        ---------------
+
+        img:numpy Array
+            Input Image
+
+        Returns
+
+        --------------
+
+        img: numpy Array
+            Image with rectangles draw around the detected classes
+        '''
         for detection in self.detections:
             x, y, w, h = detection[2][0],\
                 detection[2][1],\
@@ -154,56 +199,179 @@ class BrickZone(object):
                         [0, 255, 0], 2)
         return img
 
+
+
+    def cvDrawBoxes_test(self ,img):
+        
+        for detection in self.detections:
+            x, y, w, h = detection[2][0],\
+                detection[2][1],\
+                detection[2][2],\
+                detection[2][3]
+            xmin, ymin, xmax, ymax = self.convertBack(
+                float(x), float(y), float(w), float(h))
+            pt1 = (xmin, ymin)
+            pt2 = (xmax, ymax)
+            cv2.rectangle(img, pt1, pt2, (0, 255, 0), 1)
+            cv2.putText(img,
+                        detection[0].decode() +
+                        " [" + str(round(detection[1] * 100, 2)) + "]",
+                        (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                        [0, 255, 0], 2)
+        return img
+
     def subscribe_position(self):
+        '''
+            Subscribes to the local position topic
+            Returns
+
+            ---------------
+
+            position: list
+                    Contains x,y,z coordinates indicating the drones current position
+        '''
         drone_pos = rospy.wait_for_message('/dji_sdk/local_position',PointStamped)
         x_pos = float(drone_pos.point.x)
         y_pos = float(drone_pos.point.y)
         z_pos = float(drone_pos.point.z)
 
-        position = [x_pos,y_pos,z_pos] #Position contains the x,y,z coordinates of the drone
+        position = [x_pos,y_pos,z_pos] 
 
         return position
 
 
+    def find_bb_centroid(self,detections):
+        '''
+            Computes centroids of the BBs based on the obj_code
+            
+            Parameters
+            __________
+
+            detections: list
+                        Contains detections in the following format:  [(b'class_name', confidence score, (centre_x, centre_y, absolute_BB_width, absolute_BB_height))...]
+            Returns
+            __________
+
+            centroid_ls: list
+                        [[centroid_xcoordinate,centroid_ycoordinate]]
+                        
+        '''
+        
+        centroid_arr = np.zeros((1,2))
+        print("detections:",detections)                    
+        for detection in detections:
+            centroid_xcoor,centroid_ycoor = detection[2][0],detection[2][1]
+            centroid_arr = centroid_arr + (centroid_xcoor,centroid_ycoor)
+
+        centroid_arr = centroid_arr // len(detections)
+        centroid_ls = centroid_arr.tolist()
+        return centroid_ls
+
+    def check_obj_location(self,centroid_ls):
+        '''
+            Checks if the centroid lies within 60% of the image centre
+
+            Parameters
+
+            ------------
+            
+            centroid_ls: list
+                        [[centroid_xcoordinate,centroid_ycoordinate]]
+            Returns
+
+            ------------
+
+            bool
+                True if the centroid lies within 60% of the image centre , else False
+        '''
+        print("centroid_ls:",centroid_ls)
+        #Error checking: Check if the centroid is out of image bounds and make required corrections
+        for coord in centroid_ls:
+            if(coord[0] <=0):
+                coord[0] = 0
+            elif(coord[0] >= self.frame_rgb.shape[1]):
+                coord[0] = self.frame_rgb.shape[1]
+
+            if(coord[1] <= 0):
+                coord[1] = 0
+            elif(coord[1] >= self.frame_rgb.shape[0]):
+                coord[1] = self.frame_rgb.shape[0]
+
+
+        #Check if centroid lies within 60% of the image centre
+
+        for coord in centroid_ls:
+            if((coord[0] >= (0.20 * self.frame_rgb.shape[1])) and (coord[0] <= (0.80 * self.frame_rgb.shape[1])) and (coord[1] >= 0.20 * self.frame_rgb.shape[0]) and (coord[1] <= 0.80 * (self.frame_rgb.shape[0]))):
+                    return True
+
+            else:
+                
+                    return False
+
+            
+                
+
     def infer(self, in_image, thresh = 0.5):
         ''' 
-            Forward pass of the model for the given RGB input image. 
-            Returns: (Class_ID, Local_Position)
-                        Class_ID : An integer which maps to the enum representing construction zone and bricks
-                        {0: "Construction Zone"; 1: "Red Brick"; 2: "Blue Brick"; 3: "Green Brick"; 4: "Orange Brick"}
-                        Local_Position: A float representing the local position of the drone, this is obtained from 
-                                        subscribing to one of the ROS topics
+            Forward pass of the model for the given RGB input image.
+
+            Parameters
+            -----------------
+            in_image:numpy array 
+                    Input RGB image
         '''
+
+
+        #Local Variables
+        uav_constr_detections = []
+        centroid_ls = []
+        local_pos_ls = []
 
         self.frame_rgb = cv2.cvtColor(in_image, cv2.COLOR_BGR2RGB)
         frame_resized = self.frame_rgb
-        #frame_resized = cv2.resize(self.frame_rgb, (self.net_width, self.net_height), interpolation=cv2.INTER_LINEAR) #changed
         # Copy image to darknet format 
         darknet.copy_image_from_bytes(self.darknet_image,frame_resized.tobytes())
         # Forward pass 
-        detections = darknet.detect_image(self.netMain, self.metaMain, self.darknet_image, thresh)
-        print("detections:",detections)
+        self.detections = darknet.detect_image(self.netMain, self.metaMain, self.darknet_image, thresh)
+        print("self.detections:",self.detections)
 
-        if not detections:
+        #Drawing detections on the image
+        test_img = self.cvDrawBoxes_test(frame_resized)
+        test_img = cv2.cvtColor(test_img,cv2.COLOR_RGB2BGR)
+        cv2.imshow("Detections on the test image",test_img)
+        key = cv2.waitKey(0)
+        if(key & 0xFF == ord('q')):
+            cv2.destroyAllWindows()
+            exit()
+
+        if not self.detections:
             return []
-        # Obtain the best detection from the list of detections 
-        best_detection = detections[0]
 
-        #### Check the class_ID of the detection and based on the ID return the local_position if 
-        #### its a construction zone or do further processing if its a brick
+        else:
+            for detection in self.detections:
+                if(detection[0] == b'channel' and int(detection[1]*100)>=60):
+                    uav_constr_detections.append(detection)
 
-        if (best_detection[0] == 1):
-            return 0
-            #local_position = self.subscribe_position() #changed
-            #return (0, local_position) #changed
+                #TODO: Conditions for rest of the classes to be added once the detector is trained for them
+            
+            if(len(uav_constr_detections)>=2):
+                centroid_ls = self.find_bb_centroid(uav_constr_detections)
+                ret = self.check_obj_location(centroid_ls)
+                
+                #local_pos_ls = self.subscribe_position()
+                self.uav_constr_pos_ls.append(local_pos_ls)
 
-        elif (best_detection[0] == 0):
-            self.detections = best_detection
-            # crop the image and check the color of the image 
-            self.select_roi()
-            #local_position = self.subscribe_position() #changed
-            #return (self.color_code, local_position) #changed
-            return self.color_code
+                #Ret = True indicates that obj centroid is located within 60% of the image centre
+                if(ret == True):
+                    self.uav_constr_pos_conf.append(1) #High confidence
+                elif(ret == False):
+                    self.uav_constr_pos_conf.append(0) #Low confidence
+        
+
+        
+        print("self.uav_constr_pos_conf:",self.uav_constr_pos_conf)
+
+            
 
 #Functions for debug
 def display(img,txt):
@@ -218,13 +386,21 @@ def display(img,txt):
 
 if __name__ == '__main__':
     brickzone_obj = BrickZone() #Object of class BrickZone
-    brickzone_obj.initialize_network("/home/varghese/challenge_2/brick_train/brick_stack_construction_zone_v1/Weights/brick.cfg","/home/varghese/challenge_2/brick_train/brick_stack_construction_zone_v1/Weights/brick_2000.weights","/home/varghese/challenge_2/brick_train/brick_stack_construction_zone_v1/Weights/brick.data") #Initializing network
+    brickzone_obj.initialize_network("/home/varghese/challenge_2/brick_train/servoing_ver_3/python_scripts/brick.cfg","/home/varghese/challenge_2/brick_train/servoing_ver_3/weights/yolov3-tiny_best.weights","/home/varghese/challenge_2/brick_train/servoing_ver_3/python_scripts/brick.data") #Initializing network
 
-    cap = cv2.VideoCapture("/home/varghese/brick_data/data_nov21/cropped_videos/rosbag_3_cropped.m4v")
-    ret = True
-    while(cap.isOpened() and ret == True):
-        ret, frame = cap.read()
-        print("frame.shape",frame.shape)
-        display(frame,"Live feed")
+    #cap = cv2.VideoCapture("/home/varghese/brick_data/data_nov21/cropped_videos/rosbag_3_cropped.m4v")
+
+    #ret = True
+    #while(cap.isOpened() and ret == True):
+    #    ret, frame = cap.read()
+    #    print("frame.shape",frame.shape)
+    #    display(frame,"Live feed")
+    #    ret_val = brickzone_obj.infer(frame)
+    #    print("Ret val:",ret_val)
+
+
+    for img_path in glob.glob("/home/varghese/challenge_2/brick_train/servoing_ver_3/test_detector/*.jpg"):
+        frame = cv2.imread(img_path)
         ret_val = brickzone_obj.infer(frame)
-        print("Ret val:",ret_val)
+
+
